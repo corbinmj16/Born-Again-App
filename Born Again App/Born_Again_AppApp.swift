46e14134f47c6959a8aaf6d6be31b2bb256db4a2
Corbin <corbin.jensen@me.com>
2024-03-11 16:30:13 -0400

    Initial Commit

---
+++ Born Again App/Born_Again_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Born_Again_AppApp.swift
+//  Born Again App
+//
+//  Created by Corbin Jensen on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Born_Again_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
